80609e81c3c0214c42294724dffe40d780b35008
Sertel392 <sertel.erten@gmail.com>
2025-06-10 20:10:13 +0300

    Add files via upload

---
+++ FilmModu/build.gradle.kts
@@ -0,0 +1,13 @@
+// use an integer for version numbers
+version = 1
+
+android {
+    defaultConfig {
+        versionName = "1.0.0"
+        versionCode = 1
+    }
+}
+
+dependencies {
+    implementation("org.jsoup:jsoup:1.15.3")
+}
