f822533c92fd10e50257a5e5df026610215b9489
Sertel392 <sertel.erten@gmail.com>
2025-06-10 20:07:29 +0300

    Update FilmModuPlugin.kt

--- FilmModu/src/main/kotlin/com/Turkuaz/saklimavi/filmmodu/FilmModuPlugin.kt
+++ FilmModu/src/main/kotlin/com/Turkuaz/saklimavi/filmmodu/FilmModuPlugin.kt
@@ -1,11 +1,11 @@
-package com.asistan.emrah.filmmodu
+package com.keyiflerolsun
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class FilmModuPlugin : Plugin() {
+class FilmModuPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(FilmModu())
     }
